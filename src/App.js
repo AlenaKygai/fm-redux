e1ac3591b2ed84db3ccc6d0d45bc56dcb80c84a4
Alena.Kygai <alena.kygai@gmail.com>
2022-08-10 17:41:05 +0300

    Use reducer,store,connect

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import {connect} from 'react-redux';
 
 function App(props) {
-  const {count, dispatch} = props;
+  const {count, step, dispatch} = props;
   const increment = () => {
     const action={type:'INCREMENT'};
     dispatch(action)
@@ -10,17 +10,25 @@ function App(props) {
     const action={type:'DECREMENT'};
     dispatch(action)
   }  
+  const onChange = ({target:{value}}) => {
+    const action={
+      type:'SET_STEP',
+      newStep: Number(value)
+    };
+    dispatch(action);
+  }
   return (
     <div>
       <h2>Count:{count}</h2>
       <button onClick={increment}>+</button>
       <button onClick={decrement}>-</button>
+      <input type='number' value={step} onChange={onChange} />
     </div>
   );
 }
 
 function mapStateToProps(state){
-  return {count:state.count};
+  return {state};
 }
 // const withProps = connect(mapStateToProps);
 // const componentWithProps = withProps(App);
--- src/index.js
+++ src/index.js
@@ -5,17 +5,28 @@ import { Provider } from 'react-redux';
 import './index.css';
 import App from './App';
 
-const initialState = {count:0};
+const initialState = {
+  count:0,
+  step:1,
+};
 function reducer (state=initialState, action){
   switch(action.type){
     case 'INCREMENT':{
       return {
-        count:state.count+1
+        ...state,
+        count:state.count+state.step
       }
     }
     case 'DECREMENT':{
       return{
-        count:state.count-1
+        ...state,
+        count:state.count-state.step
+      }
+    }
+    case 'SET_STEP':{
+      return{
+        ...state,
+        step:action.newStep
       }
     }
     default:
